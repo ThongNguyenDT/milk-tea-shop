38701db76f390be1c4815470e2f2ecf8f42bfc2a
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-09-02 22:30:28 +0700

    jwt config

---
+++ src/main/java/com/example/web_project/config/webConfig.java
@@ -0,0 +1,37 @@
+package com.example.web_project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.example")
+public class webConfig implements WebMvcConfigurer {
+    @Bean
+    public InternalResourceViewResolver jspViewResolver() {
+        InternalResourceViewResolver resolver= new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/")
+                .addResourceLocations("/resources/static/")
+                .addResourceLocations("/resources/static/assets/")
+                .addResourceLocations("/resources/static/css/")
+                .addResourceLocations("/resources/static/js/")
+                .addResourceLocations("/resources/static/media/")
+                .addResourceLocations("/resources/template");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("/resources/static/");
+//        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+//        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+    }
+}
