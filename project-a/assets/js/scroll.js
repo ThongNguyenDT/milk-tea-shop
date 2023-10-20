7df04995c4b6c426cf2fff06bd78be2b6e55a905
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-10-20 22:14:14 +0700

    finish landing page

--- project-a/assets/js/scroll.js
+++ project-a/assets/js/scroll.js
@@ -1,11 +1,13 @@
-const pageHeight = 1036;
-const pages = document.querySelectorAll('.sec_3 .row');
-document.querySelector('.cus-fullpage').style.height = `${pageHeight * pages.length}px`;
+const pageHeight = window.innerHeight;
+const pages = document.querySelectorAll('.cus-fullpage-items');
+const pagePositions = document.querySelector('.sec_3 .cus-fullpage-position');
+const curPages = document.querySelector('.cus-fullpage');
+curPages.style.height = `${pageHeight * pages.length + pageHeight}px`;
 
-const stylePage = `background-image: url(https://www.koithe.com/uploads/tab-contents/59e5ac7097ee9.jpg); height: ${pageHeight}; position:fixed width: 100%; top: 0px;left: 0px; z-index: 5;`
+const stylePage = `position:fixed; top: 0px;left: 0px; z-index: 5;`
 
 let sections = document.querySelectorAll('section');
-const sec_1 = document.querySelector('.sec_1 .container');
+const sec_1 = document.querySelector('.sec_1');
 const btn_animate = document.querySelector('.shop_btn button');
 const sec_2 = document.querySelector('.slider');
 
@@ -15,27 +17,29 @@ window.onscroll = () => {
     let offset = sec.offsetTop;
     let height = sec.offsetHeight;
 
-    let earlyPoint = 250;
-    if (top < 5280) earlyPoint = 500;
+    let earlyPoint = 500;
+    if (top < sections[1].offsetTop-100) earlyPoint = 500;
     else earlyPoint = 0;
 
-    if (top >= offset-earlyPoint && top < offset + height) {
+    if (top >= offset-earlyPoint && top < offset + height && top > 16) {
       let scrollper = top / 9479;
-      console.log(top)
-      // console.log(scrollper);
-      if (scrollper <= 1) {
+      console.log('top:', top);
+      console.log('start:',offset-earlyPoint);
+      console.log('end:', offset + height);
+      console.log(scrollper);
+      if (scrollper <= 1 && (sec_1.classList.contains('show-animate') || sec_2.classList.contains('show-animate'))) {
         // container.style.transform = `translateY(${top + offset + 950}px)`;
         if (scrollper > 0.01) {
           btn_animate.classList.add('Btn_hover--active')
           let scale = 1200 * scrollper - (1200 * 0.01);
           let movX = -5250 * scrollper;
-          if (scrollper > 0.25){
-            scale = 1200 * scrollper * 2.2 - (1200 * 0.01);
-            movX = -5250 * scrollper * 2.2;
+          if (scrollper > 0.1){
+            scale = 1200 * scrollper * 5 - (1200 * 0.01);
+            movX = -5250 * scrollper * 5;
           }
-          btn_animate.style.transform = `matrix(${scale},0,0,${scale},${movX},0)`;
+            btn_animate.style.transform = `matrix(${scale},0,0,${scale},${movX},0)`;
           if (scrollper <= 0.9)
-            btn_animate.style.opacity = `${scrollper*2 + 0.11}`;
+            btn_animate.style.opacity = `${scrollper*5 + 0.11}`;
           if (scrollper > 0.98)
             btn_animate.classList.add('Btn_hover--active_last')
         }
@@ -47,17 +51,20 @@ window.onscroll = () => {
 
         }
       }
+      // if (offset-earlyPoint == 4242 && sections[2].children[0].children.length == 7 )
 
       sec.classList.add('show-animate');
     }
-    else
+    else{
       sec.classList.remove('show-animate');
+      pagePositions.style.display = 'none'
+    }
   })
 
   let index = 0;
   pages.forEach(page => {
     let top = window.scrollY
-    let offset = 6314 + pageHeight * index;
+    let offset = sections[2].offsetTop + pageHeight * index;
     index++;
     // console.log({
     //   index: `${index}`,
@@ -66,10 +73,28 @@ window.onscroll = () => {
     //   offset: `${offset}`
     // })
     if (index > pages.length) index = 0;
-    if (top >= offset && top < offset + pageHeight) {
+    if (top >= offset && top < offset + pageHeight && index < 8) {
       page.style.cssText = stylePage;
+      pagePositions.style.display = 'block'
+      // console.log('indexchi ',pagePositions.children[index-1]);
+      // console.log('indexchichi ',pagePositions.children[index-1].children[1]);
+      pagePositions.children[index-1].style.color = 'wheat';
+      pagePositions.children[index-1].children[1].style.color = 'green';
+    }
+    else
+    {
+      page.style.cssText = '';
+      pagePositions.children[index-1].style.cssText = '';
+      pagePositions.children[index-1].children[1].style.cssText = '';
     }
 
   })
 
 }
+
+function myFunction(i) {
+  const index = [4242,5278,6314,7350,8386,9422,10458,12578]
+  window.scrollTo(0, index[i]);
+}
+
+
