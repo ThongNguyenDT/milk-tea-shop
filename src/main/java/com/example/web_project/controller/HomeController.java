a14258f391567fa82b0863eeed04b97e5c71b003
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-10-31 22:37:08 +0700

    home page controller contronller

--- src/main/java/com/example/web_project/controller/HomeController.java
+++ src/main/java/com/example/web_project/controller/HomeController.java
@@ -1,22 +1,23 @@
 package com.example.web_project.controller;
 
-import com.example.web_project.services.ItemService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 @Controller
 public class HomeController {
+    @RequestMapping({"/", "/home"})
+    public String welcome(Map<String, Object> model) {
+        ArrayList<String> list = new ArrayList<>();
 
-    @Autowired
-    ItemService itemService;
+        list.add("Baseball");
+        list.add("Basketball");
+        list.add("Cricket");
 
-    @RequestMapping("/view-items")
-    public ModelAndView viewBooks(ModelAndView model) {
+        model.put("list", list);
 
-        model.setViewName("home");
-        model.addObject("items", itemService.getAll());
-        return model;
+        return "home";
     }
 }
