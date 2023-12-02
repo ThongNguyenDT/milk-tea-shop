e3046bcc1a4d74e81d9ee14ad392e6b9c279c16d
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-12-02 15:02:03 +0700

    view auth render

--- src/main/java/com/example/web_project/controller/AuthController.java
+++ src/main/java/com/example/web_project/controller/AuthController.java
@@ -1,40 +1,32 @@
 package com.example.web_project.controller;
 
-import com.example.web_project.auth.AuthenticationRequest;
 import com.example.web_project.auth.AuthenticationService;
 import com.example.web_project.auth.RegisterRequest;
-import com.example.web_project.entities.Account;
 import com.example.web_project.repository.AccountRepository;
 
+import com.example.web_project.services.securityService.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+
 import java.util.HashMap;
+
 
 @Controller
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthenticationService service;
-    @Autowired
-    private AccountRepository accountRepository;
+    private final AccountRepository accountRepository;
+    private final AuthService authService;
+    
     @GetMapping("/login**")
     public String login() {
         return "redirect:/alotra/login";
@@ -42,7 +34,7 @@ public class AuthController {
 
     @GetMapping("/alotra/login")
     public String loginPage(Model model) {
-
+        model = authService.common(model);
         return "auth/signin";
     }
 
@@ -57,6 +49,7 @@ public class AuthController {
         model.addAttribute("user", user);
         return "auth/signup";
     }
+
     @Validated
     @PostMapping("/alotra/register/save")
     public String registerHandle(@Valid @ModelAttribute("user") RegisterRequest request,
@@ -65,23 +58,22 @@ public class AuthController {
         HashMap<String, Object> log = new HashMap<String, Object>();
         log.put("type", "success");
         log.put("message", "success, confirm your gmail and then you can login");
-        var existingUserEmail = accountRepository.findByEmail(request.getEmail()).orElse(new Account());
-        if (existingUserEmail.getEmail() != null && !existingUserEmail.getEmail().isEmpty()) {
+        var existingUserEmail = accountRepository.findByEmail(request.getEmail());
+        if (existingUserEmail.isPresent() && existingUserEmail.get().getEmail() != null && !existingUserEmail.get().getEmail().isEmpty()) {
             result.rejectValue("email", "There is already a user with this email");
         }
-        var existingUserName = accountRepository.findByUsername(request.getUsername()).orElse(new Account());
-        if (existingUserName.getUsername() != null && !existingUserName.getUsername().isEmpty()) {
+        var existingUserName = accountRepository.findByUsername(request.getUsername());
+        if (existingUserName.isPresent() && existingUserName.get().getUsername() != null && !existingUserName.get().getUsername().isEmpty()) {
             result.rejectValue("username", "There is already a user with this username");
         }
-        if(!result.hasErrors()){
+        if (!result.hasErrors()) {
             service.register(request);
-        }
-        else{
+        } else {
             HashMap<String, Object> error = new HashMap<String, Object>();
             log.put("type", "error");
             log.put("message", "something wrong");
             for (Object object : result.getAllErrors()) {
-                if(object instanceof FieldError) {
+                if (object instanceof FieldError) {
                     FieldError fieldError = (FieldError) object;
                     error.put(fieldError.getField(), fieldError.getDefaultMessage());
                 }
@@ -95,8 +87,9 @@ public class AuthController {
         return "redirect:/alotra/register";
     }
 
-    @RequestMapping("/alotra/logout")
-    public String logout(){
+    @GetMapping("/alotra/logout")
+    public String logout() {
+        System.out.println("logout");
         return "redirect:/alotra";
     }
 }
