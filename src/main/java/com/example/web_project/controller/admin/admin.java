c46cca7afe84e36908587940a585e8f4704699f5
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-12-04 12:07:16 +0700

    move

--- src/main/java/com/example/web_project/controller/admin/admin.java
+++ src/main/java/com/example/web_project/controller/admin/admin.java
@@ -1,4 +1,4 @@
-package com.example.web_project.controller;
+package com.example.web_project.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
