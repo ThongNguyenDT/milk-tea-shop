055d690dcc9cc6f14728f999e1fee5fe2e9f4bfc
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2023-12-06 23:11:37 +0700

    pay page

--- src/main/java/com/example/web_project/services/ViewgiohangService.java
+++ src/main/java/com/example/web_project/services/ViewgiohangService.java
@@ -1,54 +1,19 @@
 package com.example.web_project.services;
+
 import com.example.web_project.entities.Viewgiohang;
-import com.example.web_project.repository.ViewgiohangRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class ViewgiohangService {
+public interface ViewgiohangService {
+    List<Viewgiohang> getAllViewgiohang();
 
-    private final ViewgiohangRepository viewgiohangRepository;
+    Viewgiohang getViewgiohangById(Integer id);
 
-    @Autowired
-    public ViewgiohangService(ViewgiohangRepository viewgiohangRepository) {
-        this.viewgiohangRepository = viewgiohangRepository;
-    }
+    Viewgiohang createViewgiohang(Viewgiohang viewgiohang);
 
-    public List<Viewgiohang> getAllViewgiohang() {
-        return viewgiohangRepository.findAll();
-    }
+    Viewgiohang updateViewgiohang(Integer id, Viewgiohang updatedViewgiohang);
 
-    public Viewgiohang getViewgiohangById(Integer id) {
-        return viewgiohangRepository.findById(id).orElse(null);
-    }
+    void deleteViewgiohang(Integer id);
 
-    public Viewgiohang createViewgiohang(Viewgiohang viewgiohang) {
-        return viewgiohangRepository.save(viewgiohang);
-    }
-
-    public Viewgiohang updateViewgiohang(Integer id, Viewgiohang updatedViewgiohang) {
-        Viewgiohang existingViewgiohang = viewgiohangRepository.findById(id).orElse(null);
-        if (existingViewgiohang != null) {
-            // Cập nhật thông tin của existingViewgiohang với thông tin từ updatedViewgiohang
-            existingViewgiohang.setCount(updatedViewgiohang.getCount());
-            existingViewgiohang.setDescription(updatedViewgiohang.getDescription());
-            // Cập nhật các trường khác tương tự
-            return viewgiohangRepository.save(existingViewgiohang);
-        }
-        return null;
-    }
-
-    public void deleteViewgiohang(Integer id) {
-        viewgiohangRepository.deleteById(id);
-    }
-
-    public List<Viewgiohang> getViewgiohangByBillID(Integer billID) {
-        return viewgiohangRepository.findByBillID(billID);
-    }
-
-    // Các phương thức truy vấn khác nếu cần
+    List<Viewgiohang> getViewgiohangByBillID(Integer billID);
 }
